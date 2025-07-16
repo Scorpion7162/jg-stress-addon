d5e1e497d9d09d78d72d615bec7116d8cfac4df9
Scorpion <144194359+Scorpion7162@users.noreply.github.com>
2025-07-16 21:41:34 +0100

    [ref] loads of changes
    [feat] - Add Job locking in a table to stress
    [feat] - fwork detection for esx/qb/qbx
    [optimise] - Reduce global count
    [ref] - use ox_lib for better performance:
    (lib.math.clamp/lib.table.contains)
    [optimise] - less conditional checks
    
    I Really dont know what made me do this - I was just bored :)

--- server.lua
+++ server.lua
@@ -1,33 +1,33 @@
 local resetStress = false
 
--- RegisterNetEvent('hud:server:GainStress', function(amount)
---     local src = source
---     local player = exports.qbx_core:GetPlayer(src)
---     local newStress
+--[[ RegisterNetEvent('hud:server:GainStress', function(amount)
+    local src = source
+    local player = exports.qbx_core:GetPlayer(src)
+    local newStress
     
---     if not player or (Config.stress.disableForLEO and player.PlayerData.job.type == 'leo') then return end
---     if not resetStress then
---       if not player.PlayerData.metadata.stress then
---         player.PlayerData.metadata.stress = 0
---       end
---       newStress = player.PlayerData.metadata.stress + amount
---       if newStress <= 0 then newStress = 0 end
---     else
---       newStress = 0
---     end
---     if newStress > 100 then
---       newStress = 100
---     end
+    if not player or (Config.stress.disableForLEO and player.PlayerData.job.type == 'leo') then return end
+    if not resetStress then
+      if not player.PlayerData.metadata.stress then
+        player.PlayerData.metadata.stress = 0
+      end
+      newStress = player.PlayerData.metadata.stress + amount
+      if newStress <= 0 then newStress = 0 end
+    else
+      newStress = 0
+    end
+    if newStress > 100 then
+      newStress = 100
+    end
 
---     Player(src)?.state:set("stress", newStress, true)
---     -- exports.qbx_core:Notify(src, locale('notify.stress_gain'), 'inform', 2500, nil, nil, {'#141517', '#ffffff'}, 'brain', '#C53030')
--- end)
+    Player(src)?.state:set('stress', newStress, true)
+    -- exports.qbx_core:Notify(src, locale('notify.stress_gain'), 'inform', 2500, nil, nil, {'#141517', '#ffffff'}, 'brain', '#C53030')
+end) ]]
 
 local function getStress(src)
   return Player(src)?.state?.stress or 0
 end
 
-RegisterNetEvent("hud:server:RelieveStress", function(amount)
+RegisterNetEvent('hud:server:RelieveStress', function(amount)
   local src = source
   local stress = getStress(src)
   local newStress
@@ -45,6 +45,6 @@ RegisterNetEvent("hud:server:RelieveStress", function(amount)
     newStress = 100
   end
   
-  Player(src).state:set("stress", newStress, true)
+  Player(src).state:set('stress', newStress, true)
   -- exports.qbx_core:Notify(src, locale('notify.stress_removed'), 'inform', 2500, nil, nil, {'#141517', '#ffffff'}, 'brain', '#0F52BA')
 end)
